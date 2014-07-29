41a90e1b49dc12040f1588c7dcb7c4b94a86d0dc
Paul Betts <paul@paulbetts.org>
2014-07-29 10:43:30 +0200

    This is really happening

---
+++ src/Setup/winmain.cpp
@@ -0,0 +1,13 @@
+// Setup.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include "Setup.h"
+
+int APIENTRY WinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+	return 0;
+}
