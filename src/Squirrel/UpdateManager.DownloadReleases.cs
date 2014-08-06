2ec9192ef9f32344f3ddbd3b9c82c155fcc9d928
Paul Betts <paul@paulbetts.org>
2014-08-06 17:30:15 +0200

    Hard tabs? How did you get here?

--- src/Squirrel/UpdateManager.DownloadReleases.cs
+++ src/Squirrel/UpdateManager.DownloadReleases.cs
@@ -11,62 +11,62 @@ namespace Squirrel
     {
         class DownloadReleases : IEnableLogger
         {
-			public async Task DownloadReleases(IEnumerable<ReleaseEntry> releasesToDownload, Action<int> progress = null)
-			{
-				progress = progress ?? (_ => { });
-				int current = 0;
-				int toIncrement = (int)(100.0 / releasesToDownload.Count());
+            public async Task DownloadReleases(IEnumerable<ReleaseEntry> releasesToDownload, Action<int> progress = null)
+            {
+                progress = progress ?? (_ => { });
+                int current = 0;
+                int toIncrement = (int)(100.0 / releasesToDownload.Count());
 
                 if (isHttpUrl(updateUrlOrPath)) {
-					await releasesToDownload.ForEachAsync(async x => {
-						await urlDownloader.DownloadFile(
-							String.Format("{0}/{1}", updateUrlOrPath, x.Filename),
-							Path.Combine(rootAppDirectory, "packages", x.Filename));
-						lock (progress) progress(current += toIncrement);
-					});
-				} else {
-					await releasesToDownload.ForEachAsync(x => {
-						File.Copy(
-							Path.Combine(updateUrlOrPath, x.Filename),
-							Path.Combine(rootAppDirectory, "packages", x.Filename));
-						lock (progress) progress(current += toIncrement);
-					});
-				}
-			}
+                    await releasesToDownload.ForEachAsync(async x => {
+                        await urlDownloader.DownloadFile(
+                            String.Format("{0}/{1}", updateUrlOrPath, x.Filename),
+                            Path.Combine(rootAppDirectory, "packages", x.Filename));
+                        lock (progress) progress(current += toIncrement);
+                    });
+                } else {
+                    await releasesToDownload.ForEachAsync(x => {
+                        File.Copy(
+                            Path.Combine(updateUrlOrPath, x.Filename),
+                            Path.Combine(rootAppDirectory, "packages", x.Filename));
+                        lock (progress) progress(current += toIncrement);
+                    });
+                }
+            }
 
-			Task checksumAllPackages(IEnumerable<ReleaseEntry> releasesDownloaded)
-			{
-				return releasesDownloaded.ForEachAsync(x => checksumPackage(x));
-			}
+            Task checksumAllPackages(IEnumerable<ReleaseEntry> releasesDownloaded)
+            {
+                return releasesDownloaded.ForEachAsync(x => checksumPackage(x));
+            }
 
-			void checksumPackage(ReleaseEntry downloadedRelease)
-			{
-				var targetPackage = new FileInfo(
-					Path.Combine(rootAppDirectory, "packages", downloadedRelease.Filename));
+            void checksumPackage(ReleaseEntry downloadedRelease)
+            {
+                var targetPackage = new FileInfo(
+                    Path.Combine(rootAppDirectory, "packages", downloadedRelease.Filename));
 
-				if (!targetPackage.Exists) {
-					this.Log().Error("File {0} should exist but doesn't", targetPackage.FullName);
+                if (!targetPackage.Exists) {
+                    this.Log().Error("File {0} should exist but doesn't", targetPackage.FullName);
 
-					throw new Exception("Checksummed file doesn't exist: " + targetPackage.FullName);
-				}
+                    throw new Exception("Checksummed file doesn't exist: " + targetPackage.FullName);
+                }
 
-				if (targetPackage.Length != downloadedRelease.Filesize) {
-					this.Log().Error("File Length should be {0}, is {1}", downloadedRelease.Filesize, targetPackage.Length);
-					targetPackage.Delete();
+                if (targetPackage.Length != downloadedRelease.Filesize) {
+                    this.Log().Error("File Length should be {0}, is {1}", downloadedRelease.Filesize, targetPackage.Length);
+                    targetPackage.Delete();
 
-					throw new Exception("Checksummed file size doesn't match: " + targetPackage.FullName);
-				}
+                    throw new Exception("Checksummed file size doesn't match: " + targetPackage.FullName);
+                }
 
-				using (var file = targetPackage.OpenRead()) {
-					var hash = Utility.CalculateStreamSHA1(file);
+                using (var file = targetPackage.OpenRead()) {
+                    var hash = Utility.CalculateStreamSHA1(file);
 
-					if (!hash.Equals(downloadedRelease.SHA1,StringComparison.OrdinalIgnoreCase)) {
-						this.Log().Error("File SHA1 should be {0}, is {1}", downloadedRelease.SHA1, hash);
-						targetPackage.Delete();
-						throw new Exception("Checksum doesn't match: " + targetPackage.FullName);
-					}
-				}
-			}
+                    if (!hash.Equals(downloadedRelease.SHA1,StringComparison.OrdinalIgnoreCase)) {
+                        this.Log().Error("File SHA1 should be {0}, is {1}", downloadedRelease.SHA1, hash);
+                        targetPackage.Delete();
+                        throw new Exception("Checksum doesn't match: " + targetPackage.FullName);
+                    }
+                }
+            }
         }
     }
 }
