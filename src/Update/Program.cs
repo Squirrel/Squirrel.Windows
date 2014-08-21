3b9ea34fb70b298364828202fe95ef0a845c0c04
Paul Betts <paul@paulbetts.org>
2014-08-21 15:41:43 -0700

    First hack at setting up Mono.Options

--- src/Update/Program.cs
+++ src/Update/Program.cs
@@ -3,13 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Mono.Options;
 
 namespace Update
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            if (args.Any(x => x.StartsWith("/squirrel", StringComparison.OrdinalIgnoreCase))) {
+                // NB: We're marked as Squirrel-aware, but we don't want to do
+                // anything in response to these events
+                return 0;
+            }
+
+            var opts = new OptionSet() {
+                { "h|?|help", v => ShowHelp() }
+            };
+
+            opts.Parse(args);
+
+            return 0;
+        }
+
+        static void ShowHelp()
+        {
+            Console.WriteLine("Help!");
         }
     }
 }
