e0229bb4e644fae4d64416a5a321565522dc0e3a
Paul Betts <paul@paulbetts.org>
2014-09-04 13:41:35 -0700

    Make Setup.exe suggest that we read the log

--- src/Setup/UpdateRunner.h
+++ src/Setup/UpdateRunner.h
@@ -2,7 +2,7 @@
 class CUpdateRunner
 {
 private:
-	static void DisplayErrorMessage(CString& errorMessage);
+	static void DisplayErrorMessage(CString& errorMessage, wchar_t* logFile);
 public:
 	static int ExtractUpdaterAndRun(wchar_t* lpCommandLine);
 };
