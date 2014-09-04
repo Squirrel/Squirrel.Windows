445b26650327bd09978aa594c9497af07c9ac371
Paul Betts <paul@paulbetts.org>
2014-09-04 14:53:39 -0700

    Add some extension methods to log exceptions

--- src/Squirrel/Utility.cs
+++ src/Squirrel/Utility.cs
@@ -290,6 +290,73 @@ namespace Squirrel
             Log().Error("safeDeleteFileAtNextReboot: failed - {0} - {1}", name, lastError);
         }
 
+        public static void LogIfThrows(this IFullLogger This, LogLevel level, string message, Action block)
+        {
+            try {
+                block();
+            } catch (Exception ex) {
+                switch (level) {
+                case LogLevel.Debug:
+                    This.DebugException(message ?? "", ex);
+                    break;
+                case LogLevel.Info:
+                    This.InfoException(message ?? "", ex);
+                    break;
+                case LogLevel.Warn:
+                    This.WarnException(message ?? "", ex);
+                    break;
+                case LogLevel.Error:
+                    This.ErrorException(message ?? "", ex);
+                    break;
+                }
+
+                throw;
+            }
+        }
+
+        public static async Task LogIfThrows(this IFullLogger This, LogLevel level, string message, Func<Task> block)
+        {
+            try {
+                await block();
+            } catch (Exception ex) {
+                switch (level) {
+                case LogLevel.Debug:
+                    This.DebugException(message ?? "", ex);
+                    break;
+                case LogLevel.Info:
+                    This.InfoException(message ?? "", ex);
+                    break;
+                case LogLevel.Warn:
+                    This.WarnException(message ?? "", ex);
+                    break;
+                case LogLevel.Error:
+                    This.ErrorException(message ?? "", ex);
+                    break;
+                }
+                throw;
+            }
+        }
+
+        public static void WarnIfThrows(this IEnableLogger This, Action block, string message = null)
+        {
+            This.Log().LogIfThrows(LogLevel.Warn, message, block);
+        }
+
+        public static Task WarnIfThrows(this IEnableLogger This, Func<Task> block, string message = null)
+        {
+            return This.Log().LogIfThrows(LogLevel.Warn, message, block);
+        }
+
+        public static void ErrorIfThrows(this IEnableLogger This, Action block, string message = null)
+        {
+            This.Log().LogIfThrows(LogLevel.Error, message, block);
+        }
+
+        public static Task ErrorIfThrows(this IEnableLogger This, Func<Task> block, string message = null)
+        {
+            return This.Log().LogIfThrows(LogLevel.Error, message, block);
+        }
+
         static IFullLogger logger;
         static IFullLogger Log()
         {
