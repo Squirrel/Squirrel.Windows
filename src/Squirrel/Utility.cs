f5500884c6424b99e37939febc36cfba3b6a7737
Paul Betts <paul@paulbetts.org>
2014-09-08 13:40:49 -0700

    Add more logging methods

--- src/Squirrel/Utility.cs
+++ src/Squirrel/Utility.cs
@@ -337,6 +337,29 @@ namespace Squirrel
             }
         }
 
+        public static async Task<T> LogIfThrows<T>(this IFullLogger This, LogLevel level, string message, Func<Task<T>> block)
+        {
+            try {
+                return await block();
+            } catch (Exception ex) {
+                switch (level) {
+                case LogLevel.Debug:
+                    This.DebugException(message ?? "", ex);
+                    break;
+                case LogLevel.Info:
+                    This.InfoException(message ?? "", ex);
+                    break;
+                case LogLevel.Warn:
+                    This.WarnException(message ?? "", ex);
+                    break;
+                case LogLevel.Error:
+                    This.ErrorException(message ?? "", ex);
+                    break;
+                }
+                throw;
+            }
+        }
+
         public static void WarnIfThrows(this IEnableLogger This, Action block, string message = null)
         {
             This.Log().LogIfThrows(LogLevel.Warn, message, block);
@@ -347,12 +370,22 @@ namespace Squirrel
             return This.Log().LogIfThrows(LogLevel.Warn, message, block);
         }
 
+        public static Task<T> WarnIfThrows<T>(this IEnableLogger This, Func<Task<T>> block, string message = null)
+        {
+            return This.Log().LogIfThrows(LogLevel.Warn, message, block);
+        }
+
         public static void ErrorIfThrows(this IEnableLogger This, Action block, string message = null)
         {
             This.Log().LogIfThrows(LogLevel.Error, message, block);
         }
 
         public static Task ErrorIfThrows(this IEnableLogger This, Func<Task> block, string message = null)
+        {
+            return This.Log().LogIfThrows(LogLevel.Error, message, block);
+        }
+
+        public static Task<T> ErrorIfThrows<T>(this IEnableLogger This, Func<Task<T>> block, string message = null)
         {
             return This.Log().LogIfThrows(LogLevel.Error, message, block);
         }
