391b19099d9524eaab038c2d5e50be422748c305
Paul Betts <paul@paulbetts.org>
2014-09-26 17:15:05 -0700

    Version bump to 0.5.0

--- src/Squirrel/Properties/AssemblyInfo.cs
+++ src/Squirrel/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.95")]
-[assembly: AssemblyFileVersion("0.4.95")]
+[assembly: AssemblyVersion("0.5.0")]
+[assembly: AssemblyFileVersion("0.5.0")]
 [assembly: InternalsVisibleTo("Squirrel.Tests")]
 [assembly: InternalsVisibleTo("Update")]
 
