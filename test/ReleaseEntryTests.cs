0aae942aeb66c7b3c3cbb687b14ceb3d7037b0f2
Christian Rondeau <christian.rondeau@gmail.com>
2014-09-27 15:02:39 -0400

    #48: Add tests to make sure http and network addresses are indeed accepted in ReleaseEntry parser

--- test/ReleaseEntryTests.cs
+++ test/ReleaseEntryTests.cs
@@ -10,9 +10,11 @@ namespace Squirrel.Tests.Core
     public class ReleaseEntryTests
     {
         [Theory]
-        [InlineData("94689fede03fed7ab59c24337673a27837f0c3ec  MyCoolApp-1.0.nupkg  1004502", "MyCoolApp-1.0.nupkg", 1004502)]
-        [InlineData("3a2eadd15dd984e4559f2b4d790ec8badaeb6a39  MyCoolApp-1.1.nupkg  1040561", "MyCoolApp-1.1.nupkg", 1040561)]
-        [InlineData("14db31d2647c6d2284882a2e101924a9c409ee67  MyCoolApp-1.1.nupkg.delta  80396", "MyCoolApp-1.1.nupkg.delta", 80396)]
+        [InlineData(@"94689fede03fed7ab59c24337673a27837f0c3ec  MyCoolApp-1.0.nupkg  1004502", "MyCoolApp-1.0.nupkg", 1004502)]
+        [InlineData(@"3a2eadd15dd984e4559f2b4d790ec8badaeb6a39  MyCoolApp-1.1.nupkg  1040561", "MyCoolApp-1.1.nupkg", 1040561)]
+        [InlineData(@"14db31d2647c6d2284882a2e101924a9c409ee67  MyCoolApp-1.1.nupkg.delta  80396", "MyCoolApp-1.1.nupkg.delta", 80396)]
+        [InlineData(@"0000000000000000000000000000000000000000  http://test.org/Folder/MyCoolApp-1.2.nupkg  1231953", "http://test.org/Folder/MyCoolApp-1.2.nupkg", 1231953)]
+        [InlineData(@"0000000000000000000000000000000000000000  \\Somewhere\NetworkShare\MyCoolApp-1.3.nupkg.delta  0", @"\\Somewhere\NetworkShare\MyCoolApp-1.3.nupkg.delta", 0)]
         public void ParseValidReleaseEntryLines(string releaseEntry, string fileName, long fileSize)
         {
             var fixture = ReleaseEntry.ParseReleaseEntry(releaseEntry);
