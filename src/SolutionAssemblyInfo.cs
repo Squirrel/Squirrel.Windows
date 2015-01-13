9a06fb7f8b8d459a85b47cdd62825ef70e0f5db5
Paul Betts <paul@paulbetts.org>
2015-01-13 13:14:55 -0800

    Version bump to 0.8.0

--- src/SolutionAssemblyInfo.cs
+++ src/SolutionAssemblyInfo.cs
@@ -25,4 +25,4 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("0.7.2")]
+[assembly: AssemblyVersion("0.8.0")]
