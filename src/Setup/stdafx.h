d4f3472334a40d2d3df71ab34eecec04eaa238b5
Paul Betts <paul@paulbetts.org>
2015-03-02 12:50:49 -0800

    Pretty cool how this is a thing in 2015

--- src/Setup/stdafx.h
+++ src/Setup/stdafx.h
@@ -8,8 +8,6 @@
 #include "targetver.h"
 
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
-#define UNICODE
-#define _UNICODE
 
 // Windows Header Files:
 #include <windows.h>
