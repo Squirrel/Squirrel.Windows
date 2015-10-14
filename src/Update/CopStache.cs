8bb02941bd003b70c2658ab2456ba9a7fc14bd61
Paul Betts <paul@paulbetts.org>
2015-10-14 13:07:20 -0700

    Add the worst Mustache parser in the world

---
+++ src/Update/CopStache.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Squirrel.Update
+{
+    public static class CopStache
+    {
+        public static string Render(string template, Dictionary<string, string> identifiers)
+        {
+            var buf = new StringBuilder(template);
+
+            foreach (var key in identifiers.Keys) {
+                buf.Replace("{{" + key + "}}", SecurityElement.Escape(identifiers[key]));
+            }
+
+            return buf.ToString();
+        }
+    }
+}
