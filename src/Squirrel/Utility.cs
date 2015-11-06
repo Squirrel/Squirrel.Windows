5deab75ee33eba8253468aa1f604b0b22d815bf8
Paul Betts <paul@paulbetts.org>
2015-11-06 15:46:51 -0800

    If we try to run an EXE, run it through WINE

--- src/Squirrel/Utility.cs
+++ src/Squirrel/Utility.cs
@@ -180,6 +180,9 @@ namespace Squirrel
         public static Task<Tuple<int, string>> InvokeProcessAsync(string fileName, string arguments, CancellationToken ct)
         {
             var psi = new ProcessStartInfo(fileName, arguments);
+            if (Environment.OSVersion.Platform != PlatformID.Win32NT && fileName.EndsWith (".exe", StringComparison.OrdinalIgnoreCase)) {
+                psi = new ProcessStartInfo("wine", fileName + " " + arguments);
+            }
 
             psi.UseShellExecute = false;
             psi.WindowStyle = ProcessWindowStyle.Hidden;
