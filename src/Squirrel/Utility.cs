7f07ccefdb10e804fc9a6326e47904e4e21b4af1
skaryshev <garbage4sergey@hotmail.com>
2016-01-14 10:38:23 -0800

    workingDirectory param default to empty string

--- src/Squirrel/Utility.cs
+++ src/Squirrel/Utility.cs
@@ -177,7 +177,7 @@ namespace Squirrel
             }
         }
 
-        public static Task<Tuple<int, string>> InvokeProcessAsync(string fileName, string arguments, CancellationToken ct, string workingDirectory = null)
+        public static Task<Tuple<int, string>> InvokeProcessAsync(string fileName, string arguments, CancellationToken ct, string workingDirectory = "")
         {
             var psi = new ProcessStartInfo(fileName, arguments);
             if (Environment.OSVersion.Platform != PlatformID.Win32NT && fileName.EndsWith (".exe", StringComparison.OrdinalIgnoreCase)) {
