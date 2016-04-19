ac76c0da21009787e8a7a6f7032a919146203f49
Paul Betts <paul@paulbetts.org>
2016-04-19 12:42:45 -0700

    Hey this testing thing might be not that bad

--- src/Squirrel/ReleaseEntry.cs
+++ src/Squirrel/ReleaseEntry.cs
@@ -87,7 +87,7 @@ namespace Squirrel
         }
 
         static readonly Regex entryRegex = new Regex(@"^([0-9a-fA-F]{40})\s+(\S+)\s+(\d+)[\r]*$");
-        static readonly Regex commentRegex = new Regex(@"#.*$");
+        static readonly Regex commentRegex = new Regex(@"\s*#.*$");
         static readonly Regex stagingRegex = new Regex(@"#\s+(\d{1,3})%$");
         public static ReleaseEntry ParseReleaseEntry(string entry)
         {
@@ -96,7 +96,7 @@ namespace Squirrel
             float? stagingPercentage = null;
             var m = stagingRegex.Match(entry);
             if (m != null && m.Success) {
-                stagingPercentage = Single.Parse(m.Groups[0].Value);
+                stagingPercentage = Single.Parse(m.Groups[1].Value) / 100.0f;
             }
 
             entry = commentRegex.Replace(entry, "");
