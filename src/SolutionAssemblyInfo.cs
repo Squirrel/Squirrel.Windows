45a0c1149381d3e9a5b007eca6e01c16cd6d06fb
Paul Betts <paul@paulbetts.org>
2017-06-08 12:36:28 -0700

    Version bump to 1.7.5

--- src/SolutionAssemblyInfo.cs
+++ src/SolutionAssemblyInfo.cs
@@ -25,4 +25,4 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.7.4")]
+[assembly: AssemblyVersion("1.7.5")]
