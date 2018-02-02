d3f4aa993a064405963d35580e1702b2816bceea
Raphael Schwarz <raphael@codeworx.at>
2018-02-02 16:08:12 +0100

    Added SpecialBuild handling to Setup bootstrapper for additional Environment Variable SQUIRREL_SPECIAL_BUILD on initial install.

--- src/Setup/UpdateRunner.h
+++ src/Setup/UpdateRunner.h
@@ -1,7 +1,6 @@
 #pragma once
 class CUpdateRunner
 {
-
 public:
 	static void DisplayErrorMessage(CString& errorMessage, wchar_t* logFile);
 	static HRESULT AreWeUACElevated();
@@ -9,4 +8,7 @@ public:
 	static bool DirectoryExists(wchar_t* szPath);
 	static bool DirectoryIsWritable(wchar_t* szPath);
 	static int ExtractUpdaterAndRun(wchar_t* lpCommandLine, bool useFallbackDir);
+
+protected:
+	static void CheckSpecialBuild(wchar_t* targetDir, wchar_t* commandLine);
 };
