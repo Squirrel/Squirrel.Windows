c1e8987e2a42c7124600a79b7592d67fbd488969
Paul Betts <paul@paulbetts.org>
2019-02-07 19:01:29 -0800

    Version bump to 1.9.1

--- src/SolutionAssemblyInfo.cs
+++ src/SolutionAssemblyInfo.cs
@@ -25,4 +25,4 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.9.0")]
+[assembly: AssemblyVersion("1.9.1")]
