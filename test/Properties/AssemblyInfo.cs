26c2c6ef11e011238f4571b7af77f35acde8d032
Rob Mensching <rob@firegiant.com>
2019-05-12 13:27:26 -0700

    Modernize Squirrel build process

--- test/Properties/AssemblyInfo.cs
+++ test/Properties/AssemblyInfo.cs
@@ -2,20 +2,6 @@
 using System.Runtime.InteropServices;
 using Xunit;
 
-// All other assembly info is defined in SharedAssembly.cs
-
-[assembly: AssemblyTitle("Squirrel.Tests")]
-[assembly: AssemblyProduct("Squirrel.Tests")]
-[assembly: AssemblyDescription("Squirrel.Tests")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f781bbe0-d19d-41aa-a78b-c689b1943094")]
-
 [assembly: CollectionBehavior(MaxParallelThreads=1, DisableTestParallelization=true)]
 [assembly: AssemblyMetadata("SquirrelAwareVersion", "1")]
