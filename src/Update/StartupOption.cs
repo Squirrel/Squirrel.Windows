10291def76e5b26497b51525675bad6c157648ec
Caelan Sayler <git@caesay.com>
2021-12-19 15:04:13 +0000

    Fix up description on update cli

--- src/Update/StartupOption.cs
+++ src/Update/StartupOption.cs
@@ -40,7 +40,7 @@ namespace Squirrel.Update
                 $"Usage: {exeName} command [OPTS]",
                 "",
                 "Commands:",
-                { "install=", "Install the app whose package is in the specified directory", v => { updateAction = UpdateAction.Install; target = v; } },
+                { "install=", "Install the app whose package is in the specified directory or url", v => { updateAction = UpdateAction.Install; target = v; } },
                 { "uninstall", "Uninstall the app the same dir as Update.exe", v => updateAction = UpdateAction.Uninstall},
                 { "download=", "Download the releases specified by the URL and write new results to stdout as JSON", v => { updateAction = UpdateAction.Download; target = v; } },
                 { "checkForUpdate=", "Check for one available update and writes new results to stdout as JSON", v => { updateAction = UpdateAction.CheckForUpdate; target = v; } },
@@ -48,18 +48,19 @@ namespace Squirrel.Update
                 { "createShortcut=", "Create a shortcut for the given executable name", v => { updateAction = UpdateAction.Shortcut; target = v; } },
                 { "removeShortcut=", "Remove a shortcut for the given executable name", v => { updateAction = UpdateAction.Deshortcut; target = v; } },
                 { "updateSelf=", "Copy the currently executing Update.exe into the default location", v => { updateAction =  UpdateAction.UpdateSelf; target = v; } },
-                { "processStart=", "Start an executable in the latest version of the app package", v => { updateAction =  UpdateAction.ProcessStart; processStart = v; }, true},
-                { "processStartAndWait=", "Start an executable in the latest version of the app package", v => { updateAction =  UpdateAction.ProcessStart; processStart = v; shouldWait = true; }, true},
-                { "setup=", "Does an initial install with the help of the Setup.exe resources", v => {  updateAction =  UpdateAction.Setup; target = v; }, true },
                 "",
                 "Options:",
                 { "h|?|help", "Display Help and exit", _ => {} },
                 { "i=|icon", "Path to an ICO file that will be used for icon shortcuts", v => icon = v},
                 { "s|silent", "Silent install", _ => silentInstall = true},
-                //{ "b=|baseUrl=", "Provides a base URL to prefix the RELEASES file packages with", v => baseUrl = v, true},
-                { "a=|process-start-args=", "Arguments that will be used when starting executable", v => processStartArgs = v, true},
                 { "l=|shortcut-locations=", "Comma-separated string of shortcut locations, e.g. 'Desktop,StartMenu'", v => shortcutArgs = v},
                 { "updateOnly", "Update shortcuts that already exist, rather than creating new ones", _ => onlyUpdateShortcuts = true},
+
+                // hidden arguments
+                { "processStart=", "Start an executable in the latest version of the app package", v => { updateAction = UpdateAction.ProcessStart; processStart = v; }, true},
+                { "processStartAndWait=", "Start an executable in the latest version of the app package", v => { updateAction = UpdateAction.ProcessStart; processStart = v; shouldWait = true; }, true},
+                { "a=|process-start-args=", "Arguments that will be used when starting executable", v => processStartArgs = v, true},
+                { "setup=", "Does an initial install with the help of the Setup.exe resources", v => {  updateAction = UpdateAction.Setup; target = v; }, true },
             };
 
             opts.Parse(args);
