4f5f88278288255245a0532e0d78cf2b3eff4879
Caelan Sayler <git@caesay.com>
2022-01-02 12:54:12 +0000

    Add XML comments for UpdateInfo/ChecksumFailedException

--- src/Squirrel/ChecksumFailedException.cs
+++ src/Squirrel/ChecksumFailedException.cs
@@ -2,8 +2,14 @@
 
 namespace Squirrel
 {
+    /// <summary>
+    /// Represents an error that occurs when a package does not match it's expected SHA checksum
+    /// </summary>
     public class ChecksumFailedException : Exception
     {
+        /// <summary>
+        /// The filename of the package which failed validation
+        /// </summary>
         public string Filename { get; set; }
     }
 }
--- src/Squirrel/UpdateInfo.cs
+++ src/Squirrel/UpdateInfo.cs
@@ -7,21 +7,46 @@ using Squirrel.SimpleSplat;
 
 namespace Squirrel
 {
+    /// <summary>
+    /// Holds information about the current version and pending updates, such as how many there are, and access to release notes.
+    /// </summary>
     [DataContract]
     public class UpdateInfo : IEnableLogger
     {
+        /// <summary>
+        /// The currently executing version of the application, or null if not currently installed.
+        /// </summary>
         [DataMember] public ReleaseEntry CurrentlyInstalledVersion { get; protected set; }
+
+        /// <summary>
+        /// The same as <see cref="CurrentlyInstalledVersion"/> if there are no updates available, otherwise
+        /// this will be the version that we are updating to.
+        /// </summary>
         [DataMember] public ReleaseEntry FutureReleaseEntry { get; protected set; }
+
+        /// <summary>
+        /// The list of versions between the <see cref="CurrentlyInstalledVersion"/> and <see cref="FutureReleaseEntry"/>.
+        /// These will all be applied in order.
+        /// </summary>
         [DataMember] public List<ReleaseEntry> ReleasesToApply { get; protected set; }
 
+        /// <summary>
+        /// True if the currently executing program is not currently installed
+        /// </summary>
         [IgnoreDataMember]
         public bool IsBootstrapping {
             get { return CurrentlyInstalledVersion == null; }
         }
 
+        /// <summary>
+        /// Path to folder containing local/downloaded packages
+        /// </summary>
         [IgnoreDataMember]
         public string PackageDirectory { get; protected set; }
 
+        /// <summary>
+        /// Create a new instance of <see cref="UpdateInfo"/>
+        /// </summary>
         protected UpdateInfo(ReleaseEntry currentlyInstalledVersion, IEnumerable<ReleaseEntry> releasesToApply, string packageDirectory)
         {
             // NB: When bootstrapping, CurrentlyInstalledVersion is null!
@@ -34,6 +59,9 @@ namespace Squirrel
             this.PackageDirectory = packageDirectory;
         }
 
+        /// <summary>
+        /// Retrieves all the release notes for pending packages (ie. <see cref="ReleasesToApply"/>)
+        /// </summary>
         public Dictionary<ReleaseEntry, string> FetchReleaseNotes()
         {
             return ReleasesToApply
@@ -49,6 +77,11 @@ namespace Squirrel
                 .ToDictionary(k => k.Item1, v => v.Item2);
         }
 
+        /// <summary>
+        /// Create a new <see cref="UpdateInfo"/> from a current release and a list of future releases
+        /// yet to be installed.
+        /// </summary>
+        /// <exception cref="Exception">When availableReleases is null or empty</exception>
         public static UpdateInfo Create(ReleaseEntry currentVersion, IEnumerable<ReleaseEntry> availableReleases, string packageDirectory)
         {
             Contract.Requires(availableReleases != null);
