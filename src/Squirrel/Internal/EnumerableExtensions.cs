07b486f57e3ea04e17a9a2a387c1308ea686b389
Caelan Sayler <git@caesay.com>
2022-02-23 11:43:56 +0000

    This function does not need to enumerate the whole collection. Remove unused extensions.

--- src/Squirrel/Internal/EnumerableExtensions.cs
+++ src/Squirrel/Internal/EnumerableExtensions.cs
@@ -14,26 +14,33 @@ namespace Squirrel
 
         /// <summary>
         /// Essentially just .Single() but with context aware error messages which are more helpful to a user.
-        /// Eg. "Invalid {is}. Only a single {what} expected in {in}."
+        /// Eg. "Invalid {is}. One {what} expected in {in}. None was found."
+        /// Eg. "Invalid {is}. Only a single {what} expected in {in}. There were 2 or more."
         /// </summary>
         public static T ContextualSingle<T>(this IEnumerable<T> source, string strIs, string strWhat, string strIn = null)
         {
-            var c = System.Linq.Enumerable.Count(source);
-            if (c == 0) {
-                throw new InvalidOperationException(
-                    $"Invalid {strIs}: One {strWhat} expected" +
-                    (strIn == null ? "." : $" in {strIn}.") +
-                    " None was found.");
-            }
+            T result;
+            using (var e = source.GetEnumerator()) {
+                // enumerator starts before the first element. If MoveNext is false there were no elements
+                if (!e.MoveNext()) {
+                    throw new InvalidOperationException(
+                        $"Invalid {strIs}: One {strWhat} expected" +
+                        (strIn == null ? "." : $" in {strIn}.") +
+                        " None was found.");
+                }
 
-            if (c > 1) {
-                throw new InvalidOperationException(
-                    $"Invalid {strIs}: Only a single {strWhat} expected" +
-                    (strIn == null ? "." : $" in {strIn}.") +
-                    $" Actually found: {c}.");
-            }
+                // our first result should be the value we return
+                result = e.Current;
 
-            return System.Linq.Enumerable.First(source);
+                // if MoveNext returns true twice, there were at least 2 elements, so we should also throw.
+                if (e.MoveNext()) {
+                    throw new InvalidOperationException(
+                        $"Invalid {strIs}: Only a single {strWhat} expected" +
+                        (strIn == null ? "." : $" in {strIn}.") +
+                        $" There were 2 or more.");
+                }
+            }
+            return result;
         }
 
         /// <summary>
@@ -114,163 +121,6 @@ namespace Squirrel
             }
 
             return result;
-        }
-
-        /// <summary>
-        /// Lazily invokes an action for each value in the sequence.
-        /// </summary>
-        /// <typeparam name="TSource">Source sequence element type.</typeparam>
-        /// <param name="source">Source sequence.</param>
-        /// <param name="onNext">Action to invoke for each element.</param>
-        /// <returns>Sequence exhibiting the specified side-effects upon enumeration.</returns>
-        public static IEnumerable<TSource> Do<TSource>(this IEnumerable<TSource> source, Action<TSource> onNext)
-        {
-            if (source == null) throw new ArgumentNullException("source");
-            if (onNext == null) throw new ArgumentNullException("onNext");
-
-            return DoHelper(source, onNext, _ => { }, () => { });
-        }
-
-        /// <summary>
-        /// Lazily invokes an action for each value in the sequence, and executes an action for successful termination.
-        /// </summary>
-        /// <typeparam name="TSource">Source sequence element type.</typeparam>
-        /// <param name="source">Source sequence.</param>
-        /// <param name="onNext">Action to invoke for each element.</param>
-        /// <param name="onCompleted">Action to invoke on successful termination of the sequence.</param>
-        /// <returns>Sequence exhibiting the specified side-effects upon enumeration.</returns>
-        public static IEnumerable<TSource> Do<TSource>(this IEnumerable<TSource> source, Action<TSource> onNext, Action onCompleted)
-        {
-            if (source == null) throw new ArgumentNullException("source");
-            if (onNext == null) throw new ArgumentNullException("onNext");
-            if (onCompleted == null) throw new ArgumentNullException("onCompleted");
-
-            return DoHelper(source, onNext, _ => { }, onCompleted);
-        }
-
-        /// <summary>
-        /// Lazily invokes an action for each value in the sequence, and executes an action upon exceptional termination.
-        /// </summary>
-        /// <typeparam name="TSource">Source sequence element type.</typeparam>
-        /// <param name="source">Source sequence.</param>
-        /// <param name="onNext">Action to invoke for each element.</param>
-        /// <param name="onError">Action to invoke on exceptional termination of the sequence.</param>
-        /// <returns>Sequence exhibiting the specified side-effects upon enumeration.</returns>
-        public static IEnumerable<TSource> Do<TSource>(this IEnumerable<TSource> source, Action<TSource> onNext, Action<Exception> onError)
-        {
-            if (source == null) throw new ArgumentNullException("source");
-            if (onNext == null) throw new ArgumentNullException("onNext");
-            if (onError == null) throw new ArgumentNullException("onError");
-
-            return DoHelper(source, onNext, onError, () => { });
-        }
-
-        /// <summary>
-        /// Lazily invokes an action for each value in the sequence, and executes an action upon successful or exceptional termination.
-        /// </summary>
-        /// <typeparam name="TSource">Source sequence element type.</typeparam>
-        /// <param name="source">Source sequence.</param>
-        /// <param name="onNext">Action to invoke for each element.</param>
-        /// <param name="onError">Action to invoke on exceptional termination of the sequence.</param>
-        /// <param name="onCompleted">Action to invoke on successful termination of the sequence.</param>
-        /// <returns>Sequence exhibiting the specified side-effects upon enumeration.</returns>
-        public static IEnumerable<TSource> Do<TSource>(this IEnumerable<TSource> source, Action<TSource> onNext, Action<Exception> onError, Action onCompleted)
-        {
-            if (source == null) throw new ArgumentNullException("source");
-            if (onNext == null) throw new ArgumentNullException("onNext");
-            if (onError == null) throw new ArgumentNullException("onError");
-            if (onCompleted == null) throw new ArgumentNullException("onCompleted");
-
-            return DoHelper(source, onNext, onError, onCompleted);
-        }
-
-        private static IEnumerable<TSource> DoHelper<TSource>(this IEnumerable<TSource> source, Action<TSource> onNext, Action<Exception> onError, Action onCompleted)
-        {
-            using (var e = source.GetEnumerator()) {
-                while (true) {
-                    var current = default(TSource);
-                    try {
-                        if (!e.MoveNext())
-                            break;
-
-                        current = e.Current;
-                    } catch (Exception ex) {
-                        onError(ex);
-                        throw;
-                    }
-
-                    onNext(current);
-                    yield return current;
-                }
-
-                onCompleted();
-            }
-        }
-
-        /// <summary>
-        /// Returns the source sequence prefixed with the specified value.
-        /// </summary>
-        /// <typeparam name="TSource">Source sequence element type.</typeparam>
-        /// <param name="source">Source sequence.</param>
-        /// <param name="values">Values to prefix the sequence with.</param>
-        /// <returns>Sequence starting with the specified prefix value, followed by the source sequence.</returns>
-        public static IEnumerable<TSource> StartWith<TSource>(this IEnumerable<TSource> source, params TSource[] values)
-        {
-            if (source == null) throw new ArgumentNullException("source");
-
-            return source.StartWith_(values);
-        }
-
-        static IEnumerable<TSource> StartWith_<TSource>(this IEnumerable<TSource> source, params TSource[] values)
-        {
-            foreach (var x in values) yield return x;
-
-            foreach (var item in source) yield return item;
-        }
-
-        /// <summary>
-        /// Returns elements with a distinct key value by using the default equality comparer to compare key values.
-        /// </summary>
-        /// <typeparam name="TSource">Source sequence element type.</typeparam>
-        /// <typeparam name="TKey">Key type.</typeparam>
-        /// <param name="source">Source sequence.</param>
-        /// <param name="keySelector">Key selector.</param>
-        /// <returns>Sequence that contains the elements from the source sequence with distinct key values.</returns>
-        public static IEnumerable<TSource> Distinct<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
-        {
-            if (source == null) throw new ArgumentNullException("source");
-            if (keySelector == null) throw new ArgumentNullException("keySelector");
-
-            return source.Distinct_(keySelector, EqualityComparer<TKey>.Default);
-        }
-
-        /// <summary>
-        /// Returns elements with a distinct key value by using the specified equality comparer to compare key values.
-        /// </summary>
-        /// <typeparam name="TSource">Source sequence element type.</typeparam>
-        /// <typeparam name="TKey">Key type.</typeparam>
-        /// <param name="source">Source sequence.</param>
-        /// <param name="keySelector">Key selector.</param>
-        /// <param name="comparer">Comparer used to compare key values.</param>
-        /// <returns>Sequence that contains the elements from the source sequence with distinct key values.</returns>
-        public static IEnumerable<TSource> Distinct<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector, IEqualityComparer<TKey> comparer)
-        {
-            if (source == null) throw new ArgumentNullException("source");
-            if (keySelector == null) throw new ArgumentNullException("keySelector");
-            if (comparer == null) throw new ArgumentNullException("comparer");
-
-            return source.Distinct_(keySelector, comparer);
-        }
-
-        static IEnumerable<TSource> Distinct_<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector, IEqualityComparer<TKey> comparer)
-        {
-            var set = new HashSet<TKey>(comparer);
-
-            foreach (var item in source) {
-                var key = keySelector(item);
-
-                if (set.Add(key)) yield return item;
-            }
         }
     }
 }
