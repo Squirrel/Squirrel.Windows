44340cbb3a45bb3a336da28ae8d17bce4b6b973e
Caelan Sayler <git@caesay.com>
2022-05-06 09:22:15 +0100

    re-arrange project directories; prep for xplat

--- src/Squirrel/Properties/AssemblyInfo.cs
+++ src/Squirrel/Properties/AssemblyInfo.cs
@@ -3,10 +3,11 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 [assembly: InternalsVisibleTo("Squirrel.Tests, PublicKey=" + SNK.SHA1)]
-[assembly: InternalsVisibleTo("Update, PublicKey=" + SNK.SHA1)]
-[assembly: InternalsVisibleTo("SquirrelCli, PublicKey=" + SNK.SHA1)]
-[assembly: InternalsVisibleTo("Squirrel.Shared, PublicKey=" + SNK.SHA1)]
 [assembly: InternalsVisibleTo("Squirrel, PublicKey=" + SNK.SHA1)]
+[assembly: InternalsVisibleTo("Squirrel.CommandLine, PublicKey=" + SNK.SHA1)]
+[assembly: InternalsVisibleTo("Squirrel.CommandLine.Windows, PublicKey=" + SNK.SHA1)]
+[assembly: InternalsVisibleTo("Update, PublicKey=" + SNK.SHA1)]
+[assembly: InternalsVisibleTo("Update.Windows, PublicKey=" + SNK.SHA1)]
 
 internal static class SNK
 {
